7876398b8cdf7124694666167c75ba46d98b44b1
martaaa-16 <martaprama@gmail.com>
2026-02-24 22:15:27 +0700

    praktikum 1

---
+++ Jobsheet-02/new/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '244107020205';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID $id";
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return "Nama saya ".$name;
+});
