0aef77adf3515a17ace638c31d16658689acedb3
martaaa-16 <martaprama@gmail.com>
2026-02-25 01:13:52 +0700

    tugas praktikum

---
+++ Jobsheet-02/POS/resources/views/products/baby-kid.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Baby & Kid</title>
+</head>
+<body>
+    <h1>Baby & Kid Products</h1>
+    <ul>
+        <li>Diapers</li>
+        <li>Baby Food</li>
+        <li>Toys</li>
+        <li>Clothing</li>
+    </ul>
+    <p>Explore our range of baby and kid products to find the best for your little ones!</p>
+    <a href="/">Back to Home</a>
+</body>
+</html>
---
+++ Jobsheet-02/POS/resources/views/sales.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sales</title>
+</head>
+<body>
+    <h1>Welcome to the Sales Page</h1>
+    <p>Here you can find the latest sales and promotions on our products. Don't miss out on great deals!</p>
+    <a href="/">Back to Home</a>
+</body>
+</html>
