602a76eb640a769c46d08e7bd039b096b1b7ad04
martaaa-16 <martaprama@gmail.com>
2026-03-01 14:59:22 +0700

    praktikum 4

---
+++ Jobsheet-03/POS/Jobsheet-03.md
@@ -0,0 +1,46 @@
+# LAPORAN PRAKTIKUM JOBSHEET-03
+
+## PRAKTIKUM 1
+![](img/1.png)
+
+## PRAKTIKUM 2
+### Praktikum 2.1
+![](img/2.png)
+
+### Praktikum 2.2
+![](img/3.png)
+![](img/4.png)
+![](img/5.png)
+![](img/6.png)
+![](img/7.png)
+![](img/8.png)
+![](img/9.png)
+![](img/10.png)
+
+## PRAKTIKUM 3
+![](img/11.png)
+![](img/12.png)
+![](img/13.png)
+![](img/14.png)
+![](img/15.png)
+![](img/16.png)
+![](img/17.png)
+![](img/18.png)
+
+## PRAKTIKUM 4
+### Insert/menambahkan data
+![](img/19.png)
+
+### Update/memperbarui data
+![](img/20.png)
+Note: Data level_nama di id ke-4 berubah/di-update dari Pelanggan menjadi Customer
+
+### Delete/menghapus data
+![](img/21.png)
+
+### Select/menampilkan data
+![](img/22.png)
+
+## PRAKTIKUM 5
+
+## PRAKTIKUM 6
--- Jobsheet-03/POS/routes/web.php
+++ Jobsheet-03/POS/routes/web.php
@@ -1,25 +1,32 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
-
-// 1. Home
-Route::get('/', [HomeController::class, 'index']);
-
-// 2. Products (Route Prefix Category)
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/level', [LevelController::class, 'index']);
+
+//use App\Http\Controllers\HomeController;
+//use App\Http\Controllers\ProductController;
+//use App\Http\Controllers\UserController;
+//use App\Http\Controllers\SalesController;
+
+// 1. Home
+//Route::get('/', [HomeController::class, 'index']);
+
+// 2. Products (Route Prefix Category)
+//Route::prefix('category')->group(function () {
+    //Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    //Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    //Route::get('/home-care', [ProductController::class, 'homeCare']);
+    //Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+//});
+
 // 3. User (Route Parameter)
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+//Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
 // 4. Penjualan
-Route::get('/sales', [SalesController::class, 'index']);
+//Route::get('/sales', [SalesController::class, 'index']);
